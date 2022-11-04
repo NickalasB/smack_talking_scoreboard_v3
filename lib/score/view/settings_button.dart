287cd5e69765a9337e88268a2c85ee6e92e6ed1f
Nick Bradshaw <nickalasb@gmail.com>
2022-11-04 08:01:18 -0700

    ST-2: add some common components and content to settings bottom sheet

--- lib/score/view/settings_button.dart
+++ lib/score/view/settings_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smack_talking_scoreboard_v3/l10n/l10n.dart';
 import 'package:smack_talking_scoreboard_v3/score/view/ui_components/circular_button.dart';
+import 'package:smack_talking_scoreboard_v3/score/view/ui_components/primary_button.dart';
 
 class FtwButton extends StatelessWidget {
   const FtwButton({
@@ -11,24 +12,32 @@ class FtwButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return CircularIconButton(
       key: const Key('settings_button'),
-      onTap: () {
-        showModalBottomSheet<String?>(
+      onTap: () async {
+        final text = await showModalBottomSheet<String?>(
           context: context,
           useRootNavigator: true,
-          builder: (context) => SizedBox(
-            height: 260,
-            child: Column(
-              children: const [
-                BottomSheetHeader(
-                  key: Key('settings_bottom_sheet'),
-                ),
-              ],
+          isScrollControlled: true,
+          builder: (context) => Padding(
+            padding: MediaQuery.of(context).viewInsets,
+            child: SizedBox(
+              height: 260,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  BottomSheetHeader(
+                    key: Key('settings_bottom_sheet'),
+                  ),
+                  Flexible(child: BottomSheetContent()),
+                ],
+              ),
             ),
           ),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
           ),
         );
+        print('Nick text = $text');
       },
       icon: const Icon(Icons.settings),
     );
@@ -43,6 +52,7 @@ class BottomSheetHeader extends StatelessWidget {
     final l10n = context.l10n;
 
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
         const SizedBox(height: 16),
         Padding(
@@ -55,6 +65,36 @@ class BottomSheetHeader extends StatelessWidget {
         ),
         const Divider(thickness: 2),
       ],
+    );
+  }
+}
+
+class BottomSheetContent extends StatefulWidget {
+  const BottomSheetContent({super.key});
+
+  @override
+  State<BottomSheetContent> createState() => _BottomSheetContentState();
+}
+
+class _BottomSheetContentState extends State<BottomSheetContent> {
+  late final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(controller: controller),
+          const Spacer(),
+          PrimaryButton(
+            onPressed: () => Navigator.of(context).pop(controller.text),
+            label: 'DONE',
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/score/view/ui_components/primary_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    this.isFilled = true,
+  });
+
+  final VoidCallback onPressed;
+  final String label;
+  final bool isFilled;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return MaterialButton(
+      height: kMinInteractiveDimension,
+      onPressed: onPressed,
+      color: isFilled ? theme.primaryColor : null,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+        side: BorderSide(color: theme.primaryColor),
+      ),
+      child: Text(label),
+    );
+  }
+}
