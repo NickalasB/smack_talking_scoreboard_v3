491f04956e787e65d06310b847c86150ff87d0b2
Nick Bradshaw <nickalasb@gmail.com>
2022-11-05 07:20:02 -0700

    ST-2: Add Golden test for bottom sheet

--- test/score/view/settings_button_test.dart
+++ test/score/view/settings_button_test.dart
@@ -1,12 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:smack_talking_scoreboard_v3/score/view/settings_button.dart';
 
 import '../../harness.dart';
+import 'scoreboard_page_objects.dart';
+
+final scoreboardPage = ScoreBoardPageObject();
 
 void main() {
   group('Settings Button', () {
-    testWidgets('Should look right', appHarness((given, when, then) async {
-      given.pumpWidget()
+    testGoldens(
+      'Should look right',
+      appHarness((given, when, then) async {
+        await given.pumpWidget(const SettingsButton());
 
-    }));
+        await when.userTaps(scoreboardPage.settingsButton);
+        await when.pumpAndSettle();
+        await when.idle();
+
+        await then.multiScreenGoldensMatch(
+          'settings_dialog',
+          devices: [Device.phone],
+        );
+      }),
+    );
   });
 }
