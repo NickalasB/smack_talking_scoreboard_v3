0a04b39bc11e7dbca02fc3cb126b4cee06a6e21c
Nick Bradshaw <nickalasb@gmail.com>
2022-12-09 07:37:02 -0800

    ST-16:5: change some things up to fix test coverage

--- lib/score/view/scoreboard_page_dependencies.dart
+++ lib/score/view/scoreboard_page_dependencies.dart
@@ -1,5 +1,3 @@
-// coverage:ignore-file
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smack_talking_scoreboard_v3/score/bloc/score_bloc.dart';
--- test/score/view/scoreboard_page_dependencies_test.dart
+++ test/score/view/scoreboard_page_dependencies_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:given_when_then/given_when_then.dart';
 import 'package:smack_talking_scoreboard_v3/score/bloc/scoreboard_state.dart';
 import 'package:smack_talking_scoreboard_v3/score/view/scoreboard_page_dependencies.dart';
 import 'package:smack_talking_scoreboard_v3/score/view/ui_components/primary_button.dart';
@@ -9,6 +10,31 @@ import '../../helpers/test_helpers.dart';
 import 'scoreboard_page_objects.dart';
 
 void main() {
+  testWidgets(
+    'Should NOT update widget when data is the same',
+    appHarness((given, when, then) async {
+      expect(
+        given.dependencies1.updateShouldNotify(given.dependencies1),
+        isFalse,
+      );
+    }),
+  );
+
+  testWidgets(
+    'Should update widget when data is NOT the same',
+    appHarness((given, when, then) async {
+      expect(
+        given.dependencies1.updateShouldNotify(
+          const ScoreboardPageDependencies(
+            data: _TestScoreboardDependencies(),
+            child: Placeholder(),
+          ),
+        ),
+        isTrue,
+      );
+    }),
+  );
+
   testWidgets(
     'Should launch ExitGameDialog when launchExitGameDialog called',
     appHarness((given, when, then) async {
@@ -23,6 +49,13 @@ void main() {
       then.findsWidget(exitGameDialogPage);
     }),
   );
+}
+
+extension on WidgetTestGiven<AppHarness> {
+  ScoreboardPageDependencies get dependencies1 => ScoreboardPageDependencies(
+        data: harness,
+        child: const Placeholder(),
+      );
 }
 
 class _TestScoreboardDependencies extends StatelessWidget with NavigationMixin {
